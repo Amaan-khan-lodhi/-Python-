2fd8657a81585e05b04dc0daaa3e1ac205dfd649
Amaan-khan17 <amaanlodhi809@gmail.com>
2025-11-22 00:37:30 +0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+print("     Calculator")
+print("======================")
+
+num1=int(input("Enter first Number : "))
+num2=int(input("Enter secound Number : "))
+ope=input("Which operation do you want to perform(+ , - , * , / ) : ")
+
+if ope=="+":
+    sum=num1+num2
+    print(sum)
+elif ope== "-":
+    sub=num1-num2
+    print(sub)
+elif ope== "*":
+    mul=num1*num2
+    print(mul)
+elif ope== "/":
+    div=num1/num2
+    print(div)
+else:
+    print("Try Again...")
+
+
+while i <= 5:
+    print(i * i)
